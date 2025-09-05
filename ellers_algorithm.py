c04b5df64854809125ab53ec4a08301cacd53307
kalamakadanca <kalamakadanca@seznam.cz>
2025-09-05 16:45:09 +0200

    first commit

---
+++ ellers_algorithm.py
@@ -0,0 +1,24 @@
+def main():
+    maze = ellers(num_of_row_col)
+
+level_1 = 5
+level_2 = 50
+level_3 = 100
+
+num_of_row_col = level_1
+
+def ellers(num_of_row_col):
+    frst_row = "_ " * num_of_row_col
+    return x
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# Need to make { } with [ ] in it.
+# Every [ ] has 1 row of the maze.
+# Class General() for general variables
