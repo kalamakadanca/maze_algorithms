dfb7b85fc6aa0a5621cbb7f4249c6f97da60d423
kalamakadanca <kalamakadanca@seznam.cz>
2025-09-06 23:34:06 +0200

    creating general class

--- ellers_algorithm.py
+++ ellers_algorithm.py
@@ -1,24 +1,31 @@
 def main():
+    general = General()
+    num_of_row_col = general._first_level
     maze = ellers(num_of_row_col)
+    print(maze)
 
-level_1 = 5
-level_2 = 50
-level_3 = 100
 
-num_of_row_col = level_1
+
+class General():
+    def __init__(self, first_level, second_level, third_level):
+        self._first_level = 5
+        self._second_level = 50
+        self._third_level = 500
 
 def ellers(num_of_row_col):
-    frst_row = "_ " * num_of_row_col
-    return x
+    frst_row = " _" * num_of_row_col + " "
+    return frst_row
 
 
 if __name__ == "__main__":
-    main()
+    main()  
 
 
 
 
-
+# Working idea
 # Need to make { } with [ ] in it.
 # Every [ ] has 1 row of the maze.
+
+
 # Class General() for general variables
---
+++ trying.py
@@ -0,0 +1,9 @@
+level = 1
+print(level) #1
+LEVEL = 2
+print(level) #1
+print(LEVEL) #2
+level = 3
+print(level) #3
+print(LEVEL) #2
+
